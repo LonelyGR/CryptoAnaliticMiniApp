be7406b27fe288cbd4feb9476ef871eebc15e923
Lonely <damandanik271@gmail.com>
2025-12-25 20:41:49 +0200

    bot

---
+++ bot/main.py
@@ -0,0 +1,21 @@
+import asyncio
+from aiogram import Bot, Dispatcher, types
+from aiogram.filters import CommandStart
+
+TOKEN = "8246818201:AAEnfD4po58nQg4sEzzv4W7q4vQVRYWLsP8"
+
+bot = Bot(token=TOKEN)
+dp = Dispatcher()
+
+@dp.message(CommandStart())
+async def send_welcome(message: types.Message):
+    await message.answer(
+        "Привет! {username}, добро пожаловать в нашего бота!".format(username=message.from_user.full_name)
+    )
+
+async def main():
+    print("Бот запущен...")
+    await dp.start_polling(bot)
+
+if __name__ == "__main__":
+    asyncio.run(main())
